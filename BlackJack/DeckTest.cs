54377fece4131f91327065bc69004f56d8b7f7b8
David Carter <indysoftwaredev@gmail.com>
2023-10-12 15:56:49 -0400

    Initial commit

---
+++ BlackJack/DeckTest.cs
@@ -0,0 +1,11 @@
+namespace BlackJack.UnitTests
+{
+    public class DeckTest
+    {
+        [Fact]
+        public void Tests_Are_Working()
+        {
+            Assert.True(true); 
+        }
+    }
+}
