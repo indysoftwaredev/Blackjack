883d7a884e7bd829ee66bc776e70da1c6b1fb974
David Carter <indysoftwaredev@gmail.com>
2023-10-13 09:21:05 -0400

    Implemented Hand and Hand Total feature.

---
+++ BlackJack/HandTest.cs
@@ -0,0 +1,128 @@
+﻿using Blackjack.App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack.UnitTests
+{
+    public class HandTest
+    {
+        [Fact]
+        public void Tests_Are_Working()
+        {
+            Assert.False(false);
+        }
+
+        [Fact]
+        public void HandOfAA_Equals_12()
+        {
+            Hand hand = new Hand(
+                new List<Card>
+                {
+                    new Card(1, Suit.Spades),
+                    new Card(1, Suit.Spades)
+                });
+
+            Assert.Equal(12, hand.Total);
+        }
+
+        [Fact]
+        public void HandOfAAA_Equals_13()
+        {
+            Hand hand = new Hand(
+                new List<Card>
+                {
+                    new Card(1, Suit.Spades),
+                    new Card(1, Suit.Spades),
+                    new Card(1, Suit.Spades)
+                });
+
+            Assert.Equal(13, hand.Total);
+        }
+
+        [Fact]
+        public void HandOfAAAA_Equals_14()
+        {
+            Hand hand = new Hand(
+                new List<Card>
+                {
+                    new Card(1, Suit.Spades),
+                    new Card(1, Suit.Spades),
+                    new Card(1, Suit.Spades),
+                    new Card(1, Suit.Spades)
+                });
+
+            Assert.Equal(14, hand.Total);
+        }
+
+        [Fact]
+        public void HandOfAA9_Equals_21()
+        {
+            Hand hand = new Hand(
+                new List<Card>
+                {
+                    new Card(1, Suit.Spades),
+                    new Card(1, Suit.Spades),
+                    new Card(9, Suit.Spades)
+                });
+
+            Assert.Equal(21, hand.Total);
+        }
+
+        [Fact]
+        public void HandOfAAJ_Equals_12()
+        {
+            Hand hand = new Hand(
+                new List<Card>
+                {
+                    new Card(1, Suit.Spades),
+                    new Card(1, Suit.Spades),
+                    new Card(11, Suit.Spades)
+                });
+
+            Assert.Equal(12, hand.Total);
+        }
+
+        [Fact]
+        public void HandOfAK_Equals_12()
+        {
+            Hand hand = new Hand(
+                new List<Card>
+                {
+                    new Card(1, Suit.Spades),
+                    new Card(13, Suit.Spades)
+                });
+
+            Assert.Equal(21, hand.Total);
+        }
+
+        [Fact]
+        public void HandOf22_Equals_4()
+        {
+            Hand hand = new Hand(
+                new List<Card>
+                {
+                    new Card(2, Suit.Spades),
+                    new Card(2, Suit.Spades)
+                });
+
+            Assert.Equal(4, hand.Total);
+        }
+
+        [Fact]
+        public void HandOfTQA_Equals_21()
+        {
+            Hand hand = new Hand(
+                new List<Card>
+                {
+                    new Card(10, Suit.Spades),
+                    new Card(12, Suit.Spades),
+                    new Card(1, Suit.Spades)
+                });
+
+            Assert.Equal(21, hand.Total);
+        }
+    }
+}
---
+++ Blackjack.App/Hand.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack.App
+{
+    public class Hand
+    {
+        private static int TARGET_MAX_TOTAL = 21;
+
+        public Hand(List<Card> cards)
+        {
+            Cards = cards;
+        }
+
+        public List<Card> Cards { get; }
+
+        public int Total 
+        { 
+            get
+            {
+                
+                return CalculateTotal(NumberOfAces(), BasicTotal());
+            }
+        }
+
+        private int BasicTotal()
+        {
+            int total = 0;
+            Cards.ForEach(card => total += card.Value);
+            return total;
+        }
+
+        private int CalculateTotal(int numberOfAces, int total)
+        {
+            if(numberOfAces == 0) { return total; }
+            if(total <= TARGET_MAX_TOTAL) { return total; }
+
+            total -= 10;
+            if(total > TARGET_MAX_TOTAL)
+            {
+                return CalculateTotal(--numberOfAces, total);
+            }
+
+            return total;
+            
+        }
+
+        private int NumberOfAces()
+        {
+            int count = 0;
+            Cards.ForEach(card =>
+            {
+                if (card.Rank.Equals(1))
+                {
+                    count++;
+                }
+            });
+            return count;
+        }
+    }
+}
